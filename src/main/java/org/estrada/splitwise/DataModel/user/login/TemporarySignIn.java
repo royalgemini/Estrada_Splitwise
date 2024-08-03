a478d3686c4047555dbeba60dc2367d0fba74044
suven <bistasuvendu@gmail.com>
2024-08-03 16:35:24 -0500

    added temporary signin

---
+++ src/main/java/org/estrada/splitwise/DataModel/user/login/TemporarySignIn.java
@@ -0,0 +1,27 @@
+package org.estrada.splitwise.DataModel.user.login;
+
+public class TemporarySignIn {
+    private String firstName;
+    private String lastName;
+
+    public TemporarySignIn(String firstName, String lastName) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
