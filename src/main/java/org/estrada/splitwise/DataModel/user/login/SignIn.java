b48827c430a1161c32c18a18ee8b097204bdcbf1
suven <bistasuvendu@gmail.com>
2024-08-03 16:17:07 -0500

    Added datamodel for user

---
+++ src/main/java/org/estrada/splitwise/DataModel/user/login/SignIn.java
@@ -0,0 +1,67 @@
+package org.estrada.splitwise.DataModel.user.login;
+
+public class SignIn {
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String confirmPassword;
+
+    public SignIn() {
+
+    }
+    public SignIn(String username, String password, String confirmPassword) {
+        this.username=username;
+        this.password=password;
+        this.confirmPassword=confirmPassword;
+    }
+    public SignIn(String username, String email, String phoneNumber, String password, String confirmPassword) {
+        this.username=username;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.password=password;
+        this.confirmPassword=confirmPassword;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
+
---
+++ src/test/java/org/estrada/splitwise/user/register/CreateNewUserTest.java
@@ -0,0 +1,48 @@
+package org.estrada.splitwise.user.register;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateNewUserTest {
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getConfirmPassword() {
+    }
+
+    @Test
+    void setConfirmPassword() {
+    }
+}
